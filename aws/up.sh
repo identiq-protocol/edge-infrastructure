3b21f6c271725b6e51a08be1bfbbbc83eaae4dac
avielb <aviel33@gmail.com>
2020-05-26 09:21:57 +0300

    add changes

--- aws/up.sh
+++ aws/up.sh
@@ -16,9 +16,9 @@ REGION=$(cat ${1}.tfvars | grep region | awk '{print $NF}' | sed 's/"//g')
 #------------------------------------------------------------------------------#
 
 echo -e  "$COL> Ensuring SSH key pair for worker node connectivity$NOC"
-KEY_PAIR_EXISTS=$(aws ec2  describe-key-pairs --query "KeyPairs[*].KeyName" --output text | grep edge-key-pair | wc -l)
+KEY_PAIR_EXISTS=$(aws ec2  describe-key-pairs --region "$REGION" --query "KeyPairs[*].KeyName" --output text | grep edge-key-pair | wc -l)
 if [ $KEY_PAIR_EXISTS -eq 0 ]; then
-  aws ec2 create-key-pair --key-name edge-key-pair
+  aws ec2 create-key-pair --region "$REGION" --key-name edge-key-pair
 fi
 echo -e  "$COL> Deploying CloudFormation stack (may take up to 15 minutes)...$NOC"
 aws cloudformation deploy \
@@ -31,17 +31,20 @@ aws cloudformation deploy \
       NumWorkerNodes="$NUM_WORKER_NODES" \
       WorkerNodesInstanceType="$WORKER_NODES_INSTANCE_TYPE"
 
+echo -e  "$COL> Wait for EKS to become ready (5 minutes)$NOC"
+sleep 300
+
 echo -e "\n$COL> Updating kubeconfig file...$NOC"
-aws eks update-kubeconfig --name "$STACK_NAME"
+aws eks update-kubeconfig --region "$REGION" --name "$STACK_NAME"
 
 echo -e "\n$COL> Configuring worker nodes (to join the cluster)...$NOC"
-# Get worker nodes role ARN from CloudFormation stack output
+
 arn=$(aws cloudformation describe-stacks \
+  --region "$REGION" \
   --stack-name "$STACK_NAME" \
   --query "Stacks[0].Outputs[?OutputKey=='WorkerNodesRoleArn'].OutputValue" \
   --output text)
-# Enable worker nodes to join the cluster:
-# https://docs.aws.amazon.com/eks/latest/userguide/getting-started.html#eks-create-cluster
+
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: ConfigMap
@@ -57,5 +60,4 @@ data:
         - system:nodes
 EOF
 
-echo -e "\n$COL> Almost done! Cluster will be ready when all nodes have a 'Ready' status."
-echo -e "> Check it with: kubectl get nodes --watch$NOC"
+echo -e "\n$COL> connect = aws eks update-kubeconfig --region "$REGION" --name $STACK_NAME $NOC"
