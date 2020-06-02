1c831dd445ca65759fc6fcd87ce7a65efccfb156
avielb <aviel33@gmail.com>
2020-06-02 13:51:20 +0300

    add int/ext cert for cloud formation

--- aws/main.tf
+++ aws/main.tf
@@ -36,10 +36,10 @@ output certs {
 
 internal_domain_name = ${var.internal_domain_name != "NOT_SET" ? aws_acm_certificate.int-cert[0].domain_validation_options[0].resource_record_name : "NOT_SET"}
 internal_domain_value = ${var.internal_domain_name != "NOT_SET" ? aws_acm_certificate.int-cert[0].domain_validation_options[0].resource_record_value : "NOT_SET"}
-internal_domain_value = ${var.internal_domain_name != "NOT_SET" ? aws_acm_certificate.int-cert[0].arn : "NOT_SET"}
+internal_certificate_arn = ${var.internal_domain_name != "NOT_SET" ? aws_acm_certificate.int-cert[0].arn : "NOT_SET"}
 external_domain_name = ${var.external_domain_name != "NOT_SET" ? aws_acm_certificate.ext-cert[0].domain_validation_options[0].resource_record_name : "NOT_SET"}
 external_domain_value = ${var.external_domain_name != "NOT_SET" ? aws_acm_certificate.ext-cert[0].domain_validation_options[0].resource_record_value : "NOT_SET"}
-internal_domain_value = ${var.external_domain_name != "NOT_SET" ? aws_acm_certificate.ext-cert[0].arn : "NOT_SET"}
+external_certificate_arn = ${var.external_domain_name != "NOT_SET" ? aws_acm_certificate.ext-cert[0].arn : "NOT_SET"}
 EOF
 }
 output connect {
