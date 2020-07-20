c35db7ee3b9bc671f4a90194aaf5f36f7ea3bd06
avielb <aviel33@gmail.com>
2020-07-20 13:47:54 +0300

    add verification tool

---
+++ verification-tool/init-verification-tool.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+yum install docker -y
+systemctl enable docker
+systemctl start docker
+mkfs -t xfs /dev/nvme1n1
+mkdir /data
+mount /dev/nvme1n1 /data
+mkdir -p /data/output/tmp
+mkdir -p /data/output/log
