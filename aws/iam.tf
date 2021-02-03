35c93105ee8aa4750a02decbee9ea528d90799fc
avielb <aviel33@gmail.com>
2021-02-03 13:11:12 +0200

    DOP-549

---
+++ aws/iam.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_policy" "lb_controller_policy" {
+  name = "AWSLoadBalancerControllerIAMPolicy"
+  policy = data.http.iam_policy.body
+  depends_on = [data.http.iam_policy]
+}
+
+data "http" "iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.1.0/docs/install/iam_policy.json"
+}
