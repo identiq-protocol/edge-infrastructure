5e83c812856652e490674541b955ba3af8f5b44f
Aviel Buskila <aviel33@gmail.com>
2021-05-03 11:59:54 +0300

    Fix Endpoint Service Subnets (#33)

--- private-link/endpoint/variables.tf
+++ private-link/endpoint/variables.tf
@@ -14,7 +14,15 @@ variable "region" {
 data "aws_subnet_ids" "private_subnets" {
   vpc_id = data.aws_vpc.current.id
   filter {
+    name   = "availability-zone"
+    values = data.aws_vpc_endpoint_service.member_endpoint_service.availability_zones
+  }
+  filter {
     name   = "tag:Name"
     values = ["*private*"]
   }
+}
+
+data "aws_vpc_endpoint_service" "member_endpoint_service" {
+  service_name = var.service_name
 }
