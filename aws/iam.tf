a365ac5ec31f4bbe898eac06d7aa848f3550a7fa
avielb <aviel33@gmail.com>
2021-07-08 11:12:03 +0300

    refresh lb controller permissions

--- aws/iam.tf
+++ aws/iam.tf
@@ -5,5 +5,5 @@ resource "aws_iam_policy" "lb_controller_policy" {
 }
 
 data "http" "iam_policy" {
-  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.1.0/docs/install/iam_policy.json"
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
 }
