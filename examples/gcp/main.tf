c21d1ff00aff0773d269877a66c4468c5a9dcb52
codefresh-ci <codefresh@identiq.com>
2021-08-30 16:01:31 +0000

    Automated docs, variables and tag updates

--- examples/gcp/main.tf
+++ examples/gcp/main.tf
@@ -12,7 +12,7 @@
 #}
 
 module "edge-gcp" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/gcp/?ref=0.0.39"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/gcp/?ref=0.0.40"
 
   #gcp
   region                   = var.region
