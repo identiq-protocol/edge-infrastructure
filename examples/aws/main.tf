1dfd8ce65a0621da35a14f6b7f89e012b06758f8
codefresh-ci <codefresh@identiq.com>
2021-08-31 11:41:30 +0000

    Automated docs, variables and tag updates

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 #}
 
 module "edge-aws" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=0.0.68"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=0.0.69"
   # vpc
   vpc_name                 = var.vpc_name
   vpc_cidrsubnet           = var.vpc_cidrsubnet
--- examples/gcp/main.tf
+++ examples/gcp/main.tf
@@ -12,7 +12,7 @@
 #}
 
 module "edge-gcp" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/gcp/?ref=0.0.68"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/gcp/?ref=0.0.69"
 
   #gcp
   region                   = var.region
