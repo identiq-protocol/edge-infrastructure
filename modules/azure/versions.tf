823fcf6b9c06b851812bba097105626c54965649
genadipost <genadipost@gmail.com>
2021-08-31 17:57:28 +0300

    Change azuread provider version to 1.6.0 (#85)

--- modules/azure/versions.tf
+++ modules/azure/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "=2.0.1"
+      version = "=1.6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
