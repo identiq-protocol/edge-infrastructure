1c69d03f4a956329b5a85112c404068a45f0c0be
Aviel Buskila <aviel33@gmail.com>
2021-10-19 16:43:53 +0300

    Update pluto.tfvars (#95)

---
+++ codefresh/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "" > ~/.aws/credentials
+echo "terraform {" > backend.tf
+echo "  backend \"s3\" {" >> backend.tf
+echo "    region  = \"us-east-1\"" >> backend.tf
+echo "    bucket  = \"identiq-production-terraform\"" >> backend.tf
+echo "    key     = \"dev/edge-infrastructure/$(echo `pwd` |  cut -d "/" -f 6- -)\"" >> backend.tf
+echo "    encrypt = \"true\"" >> backend.tf
+echo "  }" >> backend.tf
+echo "}" >> backend.tf
