bce18c5187c15a160c1d3021756bba69f9dbfd85
roytev <roytev@gmail.com>
2021-12-23 11:11:16 +0200

    fixes

--- modules/aws/elasticache.tf
+++ modules/aws/elasticache.tf
@@ -45,7 +45,7 @@ module "redis" {
   snapshot_retention_limit             = var.ec_snapshot_retention_limit
 }
 module "redis-new" {
-  count              = var.external_redis_migration ? 1 : 0
+  count              = var.external_redis ? 1 : 0
   source             = "git::https://github.com/cloudposse/terraform-aws-elasticache-redis.git?ref=0.40.0"
   availability_zones = var.ec_cluster_mode_enabled && var.ec_cluster_mode_creation_fix_enabled ? [] : data.aws_availability_zones.available.names
   name               = "${var.external_redis_name}-redis"
@@ -73,11 +73,11 @@ module "redis-new" {
   ]
   subnets                              = local.ec_private_subnets
   cluster_mode_enabled                 = var.ec_cluster_mode_enabled
-  cluster_mode_num_node_groups         = var.ec_cluster_mode_num_node_groups_migration
+  cluster_mode_num_node_groups         = var.ec_cluster_mode_num_node_groups
   cluster_mode_replicas_per_node_group = var.ec_cluster_mode_replicas_per_node_group
   cluster_size                         = var.ec_cluster_size
   security_group_description           = "Managed by Terraform"
-  instance_type                        = var.ec_instance_type_migration
+  instance_type                        = var.ec_instance_type
   apply_immediately                    = var.ec_apply_immediately
   automatic_failover_enabled           = var.ec_automatic_failover_enabled
   engine_version                       = var.ec_engine_version
@@ -86,7 +86,7 @@ module "redis-new" {
   transit_encryption_enabled           = var.ec_transit_encryption_enabled
   parameter                            = var.ec_parameter
   tags                                 = merge(var.tags, var.default_tags)
-  snapshot_name                        = var.ec_snapshot_name_migration
+  snapshot_name                        = var.ec_snapshot_name
   snapshot_window                      = var.ec_snapshot_window
   snapshot_retention_limit             = var.ec_snapshot_retention_limit
 }
@@ -116,7 +116,7 @@ resource "kubernetes_service" "edge_redis_service" {
   }
   spec {
     type          = "ExternalName"
-    external_name = var.external_redis_migration_apply ? module.redis-new[0].endpoint : module.redis[0].endpoint
+    external_name = module.redis[0].endpoint
   }
   depends_on = [
     module.eks,
