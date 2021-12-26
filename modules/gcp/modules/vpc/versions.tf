49ce33344b263a131c6307b7369c0c260a137ed2
roytev <roytev@gmail.com>
2021-12-26 16:26:43 +0200

    fixes

--- modules/gcp/modules/vpc/versions.tf
+++ modules/gcp/modules/vpc/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google     = {
       source  = "hashicorp/google"
-      version = "4.5.0"
+      version = "3.90.1"
     }
     //  required_version = "~> 0.12"
   }
