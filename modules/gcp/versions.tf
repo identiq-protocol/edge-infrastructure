d04acccb5e9471e4512f8eff17ff3fc3c0f0e62a
Roi <roytev@gmail.com>
2021-12-26 16:48:29 +0200

    DOP-967 (#105)
    * DOP-967
    
    * version
    
    * fixes

--- modules/gcp/versions.tf
+++ modules/gcp/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "3.90.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
     //  required_version = "~> 0.12"
   }
 }
