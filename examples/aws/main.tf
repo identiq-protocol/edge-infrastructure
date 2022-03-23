d4797d37c092139bab316b181d8af0a73531e300
Nitzan Weisberg <nitzan@identiq.com>
2022-03-23 10:28:27 +0200

    1. vars/ change reverted. 2. source module changed back. 3. code tested succefully

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 #}
 
 module "edge-aws" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=DOP-1148" # this needs to be updated with the correct branch when merged.
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=0.0.35"
 
   # vpc
   external_vpc             = var.external_vpc
