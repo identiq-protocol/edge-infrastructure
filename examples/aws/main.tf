0378be6ab5ddc98081e6a543530e646cee49c941
avielb <aviel33@gmail.com>
2022-06-29 13:56:27 +0300

    elasticache autoscaling

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -13,6 +13,7 @@ provider "aws" {
 
 module "edge-aws" {
   source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=0.0.52"
+
   # vpc
   external_vpc                = var.external_vpc
   vpc_name                    = var.vpc_name
