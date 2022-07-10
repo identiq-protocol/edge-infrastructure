3743a551b8bb153d9247bd4d97c18bf24f0e5541
codefresh-ci <codefresh@identiq.com>
2022-07-10 10:48:32 +0000

    Automated docs, variables and tag updates [skip ci]

--- examples/azure/main.tf
+++ examples/azure/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 #}
 
 module "edge-azure" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/azure/?ref=0.0.57"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/azure/?ref=0.0.58"
 
   # Azure ad
   ad_application_dispaly_name = var.ad_application_dispaly_name
--- examples/gcp/main.tf
+++ examples/gcp/main.tf
@@ -5,7 +5,7 @@
 #  }
 #}
 module "edge-gcp" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/gcp/?ref=0.0.57"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/gcp/?ref=0.0.58"
 
   #gcp
   region                   = var.region
