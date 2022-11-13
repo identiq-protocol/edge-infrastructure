068d2819c82c51cdd5a2785ed23e5780de5ea1dd
codefresh-ci <codefresh@identiq.com>
2022-11-13 09:13:43 +0000

    Automated docs, variables and tag updates [skip ci]

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 #}
 
 module "edge-aws" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=0.0.62"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=0.0.63"
 
   # vpc
   external_vpc                = var.external_vpc
