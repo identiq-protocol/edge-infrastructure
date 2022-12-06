d565769e4f8c88d5b65aba08ec62f359dadc1667
genadipost <genadipost@gmail.com>
2022-12-06 15:56:09 +0200

    add tags to pinky ingress security group (#146)

--- modules/aws/security_groups.tf
+++ modules/aws/security_groups.tf
@@ -1,3 +1,10 @@
+locals {
+
+  pinky_ingress_tags = {
+    Name = "${var.vpc_name}-pinky-ingress"
+  }
+
+}
 resource "aws_security_group" "pinky_ingress" {
   name        = "${var.vpc_name}-pinky-ingress"
   description = "Allow inbound traffic from member to idq API"
@@ -10,8 +17,7 @@ resource "aws_security_group" "pinky_ingress" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-  tags = {
-    Name = "${var.vpc_name}-pinky-ingress"
-  }
+
+  tags = merge(var.default_tags, var.tags, local.pinky_ingress_tags)
 
 }
