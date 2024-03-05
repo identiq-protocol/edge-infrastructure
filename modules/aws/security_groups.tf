821b265dc26c6c8f1da631c55c0ba15422ca8678
Aviel Buskila <aviel33@gmail.com>
2024-03-05 14:16:31 +0200

    DOP-2203 (#161)
    * DOP-2203
    
    * DOP-2203
    
    * DOP-2203

--- modules/aws/security_groups.tf
+++ modules/aws/security_groups.tf
@@ -22,3 +22,15 @@ resource "aws_security_group" "pinky_ingress" {
   tags = merge(var.default_tags, var.tags, local.pinky_ingress_tags)
 
 }
+
+resource "aws_security_group_rule" "pink_ingress" {
+  count             = length(var.pinky_ingress_rules)
+  from_port         = var.pinky_ingress_rules[count.index].from_port
+  protocol          = var.pinky_ingress_rules[count.index].protocol
+  security_group_id = aws_security_group.pinky_ingress.id
+  to_port           = var.pinky_ingress_rules[count.index].to_port
+  cidr_blocks       = var.pinky_ingress_rules[count.index].cidr_blocks
+  type              = var.pinky_ingress_rules[count.index].type
+}
+
+
