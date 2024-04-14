e450e7d44869aab59f533d429490060ecdc28cde
Genadi Postrilko <genadipost@gmail.com>
2024-04-14 17:27:17 +0300

    remove us-west-2 and ap-southeast-1 regions

--- modules/aws/endpoint.tf
+++ modules/aws/endpoint.tf
@@ -14,8 +14,6 @@ locals {
   vpc_endpoint_service_name = var.vpc_custom_service_name != "" ? var.vpc_custom_service_name : local.vpc_endpoint_service_name_map[var.region]
   vpc_endpoint_service_name_map = {
     "us-east-1" : "com.amazonaws.vpce.us-east-1.vpce-svc-0964eccd96e1f130c"
-    "us-west-2" : "com.amazonaws.vpce.us-west-2.vpce-svc-00bca346ea4e4a49a"
-    "ap-southeast-1" : "com.amazonaws.vpce.ap-southeast-1.vpce-svc-0c51fc0cfb82ba390"
     "eu-central-1" : "com.amazonaws.vpce.eu-central-1.vpce-svc-0ead3a40b72d7e586"
   }
 }
