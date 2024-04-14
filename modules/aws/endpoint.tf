a7ef5430dcf3fbffb9c3ceed0179fa69670ed847
Genadi Postrilko <genadipost@gmail.com>
2024-04-14 19:26:00 +0300

    remove_us-west-2_ap-southeast-1_regions

--- modules/aws/endpoint.tf
+++ modules/aws/endpoint.tf
@@ -12,7 +12,8 @@ locals {
   ep_vpc_id                 = var.external_vpc ? var.eks_vpc_id : module.vpc[0].vpc_id
   ep_private_subnets        = var.external_vpc ? var.eks_private_subnets : module.vpc[0].private_subnets
   vpc_endpoint_service_exists = contains(keys(local.vpc_endpoint_service_name_map), var.region)
-  vpc_endpoint_service_name = var.vpc_custom_service_name != "" && vpc_endpoint_service_exists ? var.vpc_custom_service_name : local.vpc_endpoint_service_name_map[var.region]
+  vpc_endpoint_service_name_temp  = vpc_endpoint_service_exists ? local.vpc_endpoint_service_name_map[var.region] : "dummy_endpoint_will_not_be_used"
+  vpc_endpoint_service_name = var.vpc_custom_service_name != "" && vpc_endpoint_service_exists ? var.vpc_custom_service_name : vpc_endpoint_service_name_temp
   vpc_endpoint_service_name_map = {
     "us-east-1" : "com.amazonaws.vpce.us-east-1.vpce-svc-0964eccd96e1f130c"
     "eu-central-1" : "com.amazonaws.vpce.eu-central-1.vpce-svc-0ead3a40b72d7e586"
