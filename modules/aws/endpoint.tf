5cecfe20eec0e592ec795384895729af5181b939
Genadi Postrilko <genadipost@gmail.com>
2024-04-14 19:11:25 +0300

    remove_us-west-2_ap-southeast-1_regions

--- modules/aws/endpoint.tf
+++ modules/aws/endpoint.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_endpoint" "ep" {
-  count              = contains(keys(local.vpc_endpoint_service_name_map), var.region) || var.vpc_custom_service_name != "" ? 1 : 0
+  count              = vpc_endpoint_service_exists || var.vpc_custom_service_name != "" ? 1 : 0
   service_name       = local.vpc_endpoint_service_name
   vpc_id             = local.ep_vpc_id
   vpc_endpoint_type  = var.vpc_endpoint_type
@@ -11,7 +11,8 @@ resource "aws_vpc_endpoint" "ep" {
 locals {
   ep_vpc_id                 = var.external_vpc ? var.eks_vpc_id : module.vpc[0].vpc_id
   ep_private_subnets        = var.external_vpc ? var.eks_private_subnets : module.vpc[0].private_subnets
-  vpc_endpoint_service_name = var.vpc_custom_service_name != "" ? var.vpc_custom_service_name : local.vpc_endpoint_service_name_map[var.region]
+  vpc_endpoint_service_exists = contains(keys(local.vpc_endpoint_service_name_map), var.region)
+  vpc_endpoint_service_name = var.vpc_custom_service_name != "" && vpc_endpoint_service_exists ? var.vpc_custom_service_name : local.vpc_endpoint_service_name_map[var.region]
   vpc_endpoint_service_name_map = {
     "us-east-1" : "com.amazonaws.vpce.us-east-1.vpce-svc-0964eccd96e1f130c"
     "eu-central-1" : "com.amazonaws.vpce.eu-central-1.vpce-svc-0ead3a40b72d7e586"
