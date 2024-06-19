b8bb425ca24b4f16ee0d74e31cbd845e1310fe4d
codefresh-ci <codefresh@identiq.com>
2024-06-19 11:17:52 +0000

    Automated docs, variables and tag updates [skip ci]

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -12,7 +12,7 @@ terraform {
 }
 
 module "edge-aws" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=2.0.15"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/aws/?ref=2.0.16"
 
   external_vpc                                       = var.external_vpc
   eks_vpc_id                                         = var.eks_vpc_id
--- examples/azure/main.tf
+++ examples/azure/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
 #}
 
 module "edge-azure" {
-  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/azure/?ref=2.0.15"
+  source = "git@github.com:identiq-protocol/edge-infrastructure.git//modules/azure/?ref=2.0.16"
 
   # Azure ad
   ad_application_dispaly_name = var.ad_application_dispaly_name
